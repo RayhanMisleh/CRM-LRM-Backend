0203143dafae3e53238e7758e81e0902f04545d2
Rayhan Misleh <rayhanataok@gmail.com>
2025-10-31 23:14:27 -0300

    teste4

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/app';
 
+// Vercel chama essa função passando (req, res)
+// Um app Express é ele mesmo um request handler válido.
+// Então a gente simplesmente delega req/res pro Express.
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
-	return app(req, res);
+  return app(req, res);
 }
