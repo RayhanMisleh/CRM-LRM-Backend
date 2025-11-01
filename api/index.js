6052f7624e2946ecc0f5c85cedcb6c371fdbbc8d
Rayhan Misleh <rayhanataok@gmail.com>
2025-10-31 23:15:28 -0300

    teste5

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = handler;
 const app_1 = __importDefault(require("../src/app"));
+// Vercel chama essa função passando (req, res)
+// Um app Express é ele mesmo um request handler válido.
+// Então a gente simplesmente delega req/res pro Express.
 function handler(req, res) {
     return (0, app_1.default)(req, res);
 }
