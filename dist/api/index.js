3fddb974eddb222bf1c3508eb4c122aa574058b9
Rayhan Misleh <rayhanataok@gmail.com>
2025-10-31 23:11:24 -0300

    teste3

--- dist/api/index.js
+++ dist/api/index.js
@@ -3,9 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const serverless_http_1 = __importDefault(require("serverless-http"));
-const app_1 = __importDefault(require("../src/app"));
-const handler = (0, serverless_http_1.default)(app_1.default);
-exports.handler = handler;
 exports.default = handler;
+const app_1 = __importDefault(require("../src/app"));
+function handler(req, res) {
+    return (0, app_1.default)(req, res);
+}
